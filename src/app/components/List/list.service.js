8d10db44767279f7a060c6357e47669a60bed65e
wornluki <wornluki@gmail.com>
2015-11-12 11:08:23 +0100

    Add getItems()to listService & refactor mainController

--- src/app/components/List/list.service.js
+++ src/app/components/List/list.service.js
@@ -10,7 +10,8 @@
   function listService($firebaseArray, firebaseDataService) {
 
     var service = {
-      getListsByUser: getListsByUser
+      getListsByUser: getListsByUser,
+      getItemsRef: getItemsRef
       // Party: Party
     };
 
@@ -21,18 +22,19 @@
     function getListsByUser(uid) {
       var userRef = firebaseDataService.users.child(uid).child('lists');
       var vm = [];
-      var ref = "";
       userRef.on('child_added', function(snapshot) {
         var listKey = snapshot.key();
         firebaseDataService.lists.child(listKey).once('value', function(snapshot) {
           var a = snapshot.exportVal();
           console.log(a["name"])
-          vm.push(a["name"]);      
+          vm.push(a["name"]);
         })
       });
       return vm;
+    }
 
-      // return $firebaseArray(firebaseDataService.users.child(uid).child('lists'));
+    function getItemsRef(select) {
+      return firebaseDataService.lists.child(select).child('items');
     }
 
     // function Party() {
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,73 +8,80 @@
       function MainController($scope, $firebaseArray, $state, firebaseDataService, listService, currentAuth, Auth) {
 
         var vm = this;
-
+        auth();
         vm.selectAction = selectAction;
         vm.addItem = addItem;
         vm.select = "Lista1";
         vm.lists = listService.getListsByUser(currentAuth.uid);
         vm.items = $firebaseArray(firebaseDataService.lists.child(vm.select).child('items'));
-        
-        console.log(vm.lists)
+        vm.addList = addList;
+        vm.logout = logout;
+        // console.log(vm.lists)
 
-        var ref = new Firebase("hhttps://vivid-torch-6869.firebaseio.com");
-        var listRef = ref.child('lists');
-        $scope.select = "Lista1";
-        var itemsRef = ref.child('lists').child($scope.select).child('items');
-        var userRef = ref.child('users/'+currentAuth.uid+'/lists');
-        $scope.lists = [];
-        $scope.items = $firebaseArray(itemsRef);
+        // var ref = new Firebase("hhttps://vivid-torch-6869.firebaseio.com");
+        // var listRef = ref.child('lists');
+        // $scope.select = "Lista1";
+        // var itemsRef = ref.child('lists').child($scope.select).child('items');
+        // var userRef = ref.child('users/'+currentAuth.uid+'/lists');
+        // $scope.lists = [];
+        // $scope.items = $firebaseArray(itemsRef);
 
         // // Working code
         // userRef.on('child_added', function(snapshot) {
         //   var listKey = snapshot.key();
-        //   listRef.child(listKey).once('value', function(snapshot) {     
+        //   listRef.child(listKey).once('value', function(snapshot) {
         //     var a = snapshot.val();
         //     $scope.lists.push(a);
-        //   });  
+        //   });
         // });
 
 
-        //////////
+        ////////// Functions //
 
 
         function selectAction() {
-          itemsRef = ref.child('lists').child($scope.select).child('items');
-          $scope.items = $firebaseArray(itemsRef);
-
+          vm.items = $firebaseArray(firebaseDataService.lists.child(vm.select).child('items'));
+          console.log(vm.items)
         };
 
         function addItem() {
-
-          itemsRef.child($scope.nameText).set({
+          var iRef = listService.getItemsRef(vm.select);
+          // itemsRef.child($scope.nameText).set({
+          //   name: $scope.nameText,
+          //   quanity: $scope.quanityText
+          // });
+          console.log(vm.items);
+          iRef.child($scope.nameText).set({
             name: $scope.nameText,
             quanity: $scope.quanityText
-          });
+          })
 
           $scope.nameListText = "";
           $scope.nameText = "";
           $scope.quanityText = "";
         };
 
-        $scope.addList = function() {
-          listRef.child($scope.nameListText).set({
+        function addList() {
+          firebaseDataService.lists.child($scope.nameListText).set({
             name: $scope.nameListText,
             items: []
           });
-          userRef.child($scope.nameListText).set(true);
+          firebaseDataService.users.child(currentAuth.uid).child('lists').child($scope.nameListText).set(true);
           $scope.nameListText = "";
-
         };
 
-        $scope.auth = Auth;
-        $scope.auth.$onAuth(function(authData) {
-          $scope.authData = authData;
-        });
-
-        $scope.logout = function() {
+        function logout() {
           Auth.$unauth();
           $state.go('login');
+        }
+
+        function auth() {
+          $scope.auth = Auth;
+          $scope.auth.$onAuth(function(authData) {
+            $scope.authData = authData;
+          });
         };
-        
+
+
       }
   })();
